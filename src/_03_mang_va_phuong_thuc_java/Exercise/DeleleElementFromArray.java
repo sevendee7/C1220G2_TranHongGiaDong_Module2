1826e042cb8d64c917a33d2980638cacb8108fd1
sevendee7 <dongseven95@gmail.com>
2021-02-04 11:33:53 +0700

    update bài DeleteElementFromArray tối ưu hơn

--- src/_03_mang_va_phuong_thuc_java/Exercise/DeleleElementFromArray.java
+++ src/_03_mang_va_phuong_thuc_java/Exercise/DeleleElementFromArray.java
@@ -14,33 +14,44 @@ public class DeleleElementFromArray {
         }
         System.out.println("Nhập giá trị cần xóa trong mảng: ");
         int xoa = nhap.nextInt();
-        byte index = 0;
-        boolean kiemTra = false;
+        boolean timThay = false;
         for (byte i = 0; i < n; i++) {
             if (xoa == mang[i]) {
-                kiemTra = true;
-                index = i;
-                break;
+                timThay = true;
             }
         }
-        if (kiemTra) {
+        if (timThay) {
             System.out.println("Mảng trước khi xóa !");
-            for (byte i = 0; i < n; i++) {
-                System.out.print(mang[i] + "   ");
+            for (byte j = 0; j < n; j++) {
+                System.out.print(mang[j] + "   ");
             }
-            System.out.println();
-            for (byte i = index; i < mang.length-1; i++) {
-                int temp = mang[i];
-                mang[i] = mang[i+1];
-                mang[i+1] = temp;
+            byte index = 0;
+            boolean kiemTra = false;
+            do {
+                for (byte k = 0; k < n; k++) {
+                    if (xoa == mang[k]) {
+                        kiemTra = true;
+                        index = k;
+                        break;
+                    } else {
+                        kiemTra = false;
+                    }
+                }
+                if (kiemTra) {
+                    for (byte o = index; o < mang.length - 1; o++) {
+                        int temp = mang[o];
+                        mang[o] = mang[o + 1];
+                        mang[o + 1] = temp;
+                    }
+                    mang[mang.length - 1] = 0;
+                }
+            } while (kiemTra);
+            System.out.println("\nMảng sau khi xóa !");
+            for (byte p = 0; p < n; p++) {
+                System.out.print(mang[p] + "   ");
             }
-            mang[mang.length-1] = 0;
-            System.out.println("Mảng sau khi xóa !");
-            for (byte i = 0; i < n; i++) {
-                System.out.print(mang[i] + "   ");
-            }
-        }   else {
-                System.out.println("Không tìm thấy " + xoa + " trong mảng");
+        } else {
+            System.out.println("Không tìm thấy " + xoa + " trong mảng");
         }
     }
 }
