a73890ebc244c5c74575a059cd5d88f2b910e58d
sevendee7 <dongseven95@gmail.com>
2021-02-22 10:22:56 +0700

    update lại Circle.java

--- src/_05_access_modifier_static_method_static_property/Exercise/Circle.java
+++ src/_05_access_modifier_static_method_static_property/Exercise/Circle.java
@@ -1,14 +1,15 @@
 package _05_access_modifier_static_method_static_property.Exercise;
 
 public class Circle {
-    private static final double RADIUSCONST = 1.0;
-    private static final String COLORCONST = "red";
+    private static final double RADIUS_CONST = 1.0;
+    private static final String COLOR_CONST = "red";
+    private static final double PI = 3.14;
     private double radius;
     private String color;
 
     public Circle() {
-        this.radius = RADIUSCONST;
-        this.color = COLORCONST;
+        this.radius = RADIUS_CONST;
+        this.color = COLOR_CONST;
     }
 
     public Circle(double radius) {
@@ -21,5 +22,16 @@ public class Circle {
 
     public String getColor() {
         return color;
+    }
+
+    public double getArea() {
+        return radius * radius * PI;
+    }
+
+    public static void main(String[] args) {
+        Circle example = new Circle();
+        System.out.println("Radius = " + example.getRadius());
+        System.out.println("Color = " + example.getColor());
+        System.out.println("Area = " + example.getArea());
     }
 }
