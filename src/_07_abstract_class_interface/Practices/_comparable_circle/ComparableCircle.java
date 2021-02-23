9fb1869136b7fa2e0810614392c6bdc0e73b3265
sevendee7 <dongseven95@gmail.com>
2021-02-23 15:00:04 +0700

    add bài thực hành Comparable & Comparator

---
+++ src/_07_abstract_class_interface/Practices/_comparable_circle/ComparableCircle.java
@@ -0,0 +1,25 @@
+package _07_abstract_class_interface.Practices._comparable_circle;
+
+import _06_ke_thua.Practice.Circle;
+
+public class ComparableCircle extends Circle
+        implements Comparable<ComparableCircle> {
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
---
+++ src/_07_abstract_class_interface/Practices/_comparator_circle/CircleComparator.java
@@ -0,0 +1,15 @@
+package _07_abstract_class_interface.Practices._comparator_circle;
+
+import _06_ke_thua.Practice.Circle;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
