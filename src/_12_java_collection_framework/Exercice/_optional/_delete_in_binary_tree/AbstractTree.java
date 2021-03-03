dee0cd65225ed7f1d19d0f7cb33ec4489e6f9f33
sevendee7 <dongseven95@gmail.com>
2021-03-03 19:12:35 +0700

    update bài tập & thực hành optional

--- src/_12_java_collection_framework/Exercice/_optional/_delete_in_binary_tree/AbstractTree.java
+++
@@ -1,6 +0,0 @@
-package _12_java_collection_framework.Exercice._optional._delete_in_binary_tree;
-
-public abstract class AbstractTree<E> implements Tree<E> {
-    @Override
-    public void inorder() {}
-}
