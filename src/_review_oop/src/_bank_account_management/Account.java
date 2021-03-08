96cd5408ef01910e67e7024b66d121bee7b2c7a8
sevendee7 <dongseven95@gmail.com>
2021-03-08 16:19:18 +0700

    add bank

--- src/_review_oop/src/_bank_account_management/Account.java
+++ src/_review_oop/src/_bank_account_management/Account.java
@@ -1,4 +1,52 @@
 package _review_oop.src._bank_account_management;
 
 public class Account {
+    private long accountNumber;
+    private String accountName;
+    private double accountBalance;
+    
+    public void Account() {}
+
+    public void Account(long accountNumber, String accountName, double accountBalance) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.accountBalance = accountBalance;
+    }
+
+    public double Account(long accountNumber, String accountName) {
+        this.accountBalance = 50;
+        return accountBalance;
+    }
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber=" + accountNumber +
+                ", accountName='" + accountName + '\'' +
+                ", accountBalance=" + accountBalance +
+                '}';
+    }
 }
--- src/_review_oop/src/_bank_account_management/AccountManager.java
+++ src/_review_oop/src/_bank_account_management/AccountManager.java
@@ -1,4 +1,11 @@
 package _review_oop.src._bank_account_management;
 
+import java.util.List;
+
 public class AccountManager {
+    static final double INTEREST = 0.035;
+    List<Account> accountList;
+
+    public void AccountManager() {}
+
 }
